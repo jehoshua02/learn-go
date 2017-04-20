3828ce29d2504a4d7fe954ffb6d48a17993cebcc
Joshua Stoutenburg <jehoshua02@gmail.com>
2017-04-19 20:47:23 -0600

    Reverse.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/jehoshua02/learn-go/stringutil"
+)
 
 func main() {
-  fmt.Printf("Hello, world.\n")
+  fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
